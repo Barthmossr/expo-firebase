daccf03899276723aa1e2e99668d676cd934dcc7
Barthmossr <barthmossr@gmail.com>
2025-12-27 13:43:57 -0300

    chore: update react native mobile ads lib

--- src/ads/banner.tsx
+++ src/ads/banner.tsx
@@ -4,7 +4,9 @@ import type { BannerProps } from './banner.types'
 
 const Banner: React.FC<BannerProps> = ({ unitId }) => {
   const adUnitId = unitId || TestIds.BANNER
-  return <BannerAd unitId={adUnitId} size={BannerAdSize.BANNER} />
+  return (
+    <BannerAd unitId={adUnitId} size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER} />
+  )
 }
 
 export { Banner }
