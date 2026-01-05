fd24028c36a8353bc008dedb5ce70444c4f8f5e2
Barthmossr <barthmossr@gmail.com>
2026-01-05 20:16:18 -0300

    test: ensure TextInput renders with label

--- tests/unit/components/ui/text-input/text-input.spec.tsx
+++ tests/unit/components/ui/text-input/text-input.spec.tsx
@@ -10,5 +10,19 @@ describe('TextInput', () => {
 
       expect(getByTestId('text-input')).toBeDefined()
     })
+
+    it('should render with label', () => {
+      const { getByText, getByTestId } = render(
+        <TextInput
+          testID="text-input"
+          label="Email"
+          value=""
+          onChangeText={jest.fn()}
+        />,
+      )
+
+      expect(getByText('Email')).toBeDefined()
+      expect(getByTestId('text-input')).toBeDefined()
+    })
   })
 })
