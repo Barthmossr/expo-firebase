768cbd5eadff375017654ecbaced535ce4e04a5b
Barthmossr <barthmossr@gmail.com>
2026-01-05 20:07:28 -0300

    test: ensure Button renders loading indicator when loading

--- tests/unit/components/ui/button/button.spec.tsx
+++ tests/unit/components/ui/button/button.spec.tsx
@@ -10,5 +10,15 @@ describe('Button', () => {
 
       expect(getByText('Click Me')).toBeDefined()
     })
+
+    it('should render loading indicator when loading', () => {
+      const { queryByText, UNSAFE_getByType } = render(
+        <Button title="Click Me" onPress={jest.fn()} loading />,
+      )
+
+      expect(queryByText('Click Me')).toBeNull()
+      const ActivityIndicator = require('react-native').ActivityIndicator
+      expect(UNSAFE_getByType(ActivityIndicator)).toBeDefined()
+    })
   })
 })
