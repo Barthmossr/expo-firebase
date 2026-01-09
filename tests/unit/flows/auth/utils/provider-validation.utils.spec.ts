9c326ce292d277c148b504f391d43c7c974d0fe2
Barthmossr <barthmossr@gmail.com>
2026-01-08 22:36:19 -0300

    test: ensure validatePasswordProvider allows password login when user does not exists

---
+++ tests/unit/flows/auth/utils/provider-validation.utils.spec.ts
@@ -0,0 +1,25 @@
+import {
+  validatePasswordProvider,
+  validatePasswordReset,
+} from '@/flows/auth/utils/provider-validation.utils'
+import type { SignInMethodsResult } from '@/core/ports/auth'
+
+describe('provider-validation.utils', () => {
+  describe('validatePasswordProvider', () => {
+    it('should allow password login when user does not exist', () => {
+      const signInMethods: SignInMethodsResult = {
+        methods: [],
+        hasPassword: false,
+        hasOAuth: false,
+      }
+
+      const result = validatePasswordProvider(signInMethods)
+
+      expect(result).toEqual({
+        canUsePassword: true,
+        shouldUseOAuth: false,
+        message: null,
+      })
+    })
+  })
+})
