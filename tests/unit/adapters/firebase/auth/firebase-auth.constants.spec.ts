d5d69a510d7b2c105b18d202ada5cfe0d62b9ebe
Barthmossr <barthmossr@gmail.com>
2026-01-08 22:32:29 -0300

    test: ensure getActionCodeSettings returns action code settings when all config is present

---
+++ tests/unit/adapters/firebase/auth/firebase-auth.constants.spec.ts
@@ -0,0 +1,44 @@
+import Constants from 'expo-constants'
+import { randUuid, randProductName } from '@ngneat/falso'
+import {
+  getActionCodeSettings,
+  resetCache,
+} from '@/adapters/firebase/auth/firebase-auth.constants'
+
+jest.mock('expo-constants')
+
+const mockConstants = Constants as jest.Mocked<typeof Constants>
+
+describe('getActionCodeSettings', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+    resetCache()
+  })
+
+  it('should return action code settings when all config is present', () => {
+    const bundleId = `com.${randProductName().toLowerCase()}.app`
+    const packageName = `com.${randProductName().toLowerCase()}.app`
+    const projectId = randUuid()
+
+    mockConstants.expoConfig = {
+      ios: { bundleIdentifier: bundleId },
+      android: { package: packageName },
+      extra: { firebase: { projectId } },
+    } as unknown as typeof mockConstants.expoConfig
+
+    const settings = getActionCodeSettings()
+
+    expect(settings).toEqual({
+      url: `https://${projectId}.firebaseapp.com/reset-password`,
+      handleCodeInApp: true,
+      iOS: {
+        bundleId,
+      },
+      android: {
+        packageName,
+        installApp: true,
+        minimumVersion: '1.0.0',
+      },
+    })
+  })
+})
